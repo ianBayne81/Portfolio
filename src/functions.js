86c33f7bfafd98699ea7e7aff8fb21c82ada705d
Ian Bayne <43926592+ianBayne81@users.noreply.github.com>
2019-02-01 17:38:05 +1000

    updates
    linkedin icon for footer, page display, javascript span, css styling

---
+++ src/functions.js
@@ -0,0 +1,39 @@
+//aboutme button appears over video element
+const aboutMeSelect = document.querySelector('#about-me-container')
+const aboutMeButton = document.createElement('button')
+aboutMeButton.textContent = 'learn more about me'
+aboutMeButton.classList.add('about-me-button')
+
+const renderLearnMoreButton = () => {
+    aboutMeSelect.appendChild(aboutMeButton)
+}
+
+//upon click of renderLearnMoreButton this span will appear below video element 
+const aboutMeEl = document.querySelector('#more-span')
+const aboutMeSpan = document.createElement('span')
+
+const aboutMePara = document.createElement('p')
+aboutMePara.textContent = 'My family and I reside in Townsville, North Queensland, Australia. \
+ I have spent the past 15 years in the transport and logistics industry \
+ working in close teams completing operational, supervisor and manager level roles respectively. \
+ My passion for coding and learning new web development skills has helped me achieve the deployment of this \
+ web development portfolio displaying the use of html, CSS and Javascript languages confidently.\
+ My goal is to successfully change careers into web development to utilise and further develop my skills \
+ and create exciting opportunities. Please enjoy.'
+
+const minimiseButt = document.createElement('button')
+minimiseButt.textContent = 'close'
+minimiseButt.classList.add('close-span-button')
+
+const initiateSpanText = () => {
+    aboutMeEl.appendChild(aboutMeSpan)
+    aboutMeSpan.appendChild(aboutMePara)
+    aboutMeSpan.appendChild(minimiseButt)
+}
+
+minimiseButt.addEventListener("click", (e) => {
+    aboutMeEl.innerHTML = ''
+})
+
+
+export { renderLearnMoreButton, initiateSpanText }
--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
-//create a white neat button to appear in section labelled "learn more about Ian"
+import { renderLearnMoreButton, initiateSpanText } from './functions.js'
 
-// when clicked a span is activated on top of the bubble video backgroungd,
+renderLearnMoreButton()
 
-// also entailing another button to minimise span back to the initial button display
-
-
-//about-me element
-
-const aboutMe = document.querySelector('#about-me-container')
-
-const aboutMeSpan = document.createElement('span')
-
-
-const aboutMeButton = document.createElement('button')
-aboutMeButton.textContent = 'learn more about me'
-
-
-const aboutMePara = document.createElement('p')
-
-const renderAboutMeButton = () => {
-    aboutMe.appendChild(aboutMeButton)
-}
-
-renderAboutMeButton()
-
-document.querySelector('button').addEventListener(click, (e) => {
-    aboutMe.innerHTML = ''
-    
+document.querySelector('.about-me-button').addEventListener("click", (e) => {
+    initiateSpanText()
 })
+
+
+
+
