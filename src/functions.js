540368262349f7f25b9b97b0445bff0935962967
Ian Bayne <43926592+ianBayne81@users.noreply.github.com>
2019-02-04 15:52:14 +1000

    update
    styles, add resume

--- src/functions.js
+++ src/functions.js
@@ -14,12 +14,12 @@ const aboutMeSpan = document.createElement('span')
 
 const aboutMePara = document.createElement('p')
 aboutMePara.textContent = 'My family and I reside in Townsville, North Queensland, Australia. \
- I have spent the past 15 years in the transport and logistics industry \
+ I have spent the past 18 years in the transport and logistics industry \
  working in close teams completing operational, supervisor and manager level roles respectively. \
  My passion for coding and learning new web development skills has helped me achieve the deployment of this \
- web development portfolio displaying the use of html, CSS and Javascript languages confidently.\
- My goal is to successfully change careers into web development to utilise and further develop my skills \
- and create exciting opportunities. Please enjoy.'
+ web development portfolio displaying the use of html, CSS and Javascript languages supported with node.js modules \
+ and webpack configuration. My goal is to utilise these skills in a full time role continually learning and contributing to \
+ create exciting opportunities. Please enjoy.'
 
 const minimiseButt = document.createElement('button')
 minimiseButt.textContent = 'close'
