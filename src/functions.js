27460d2dd9d0ebf3433ea929bf44367ccb68de77
Ian Bayne <43926592+ianBayne81@users.noreply.github.com>
2020-01-20 13:44:27 +1000

    change theme
    change theme and add heading and button affects

--- src/functions.js
+++ src/functions.js
@@ -14,12 +14,12 @@ const aboutMeSpan = document.createElement('span')
 
 const aboutMePara = document.createElement('p')
 aboutMePara.textContent = 'My family and I reside in Townsville, North Queensland, Australia. \
- I have spent the past 18 years in the transport and logistics industry \
+ I have spent the past 22 years in the transport and logistics industry \
  working in close teams completing operational, supervisor and manager level roles respectively. \
  My passion for coding and learning new web development skills has helped me achieve the deployment of this \
- web development portfolio displaying the use of html, CSS and Javascript languages supported with node.js modules \
- and webpack configuration. My goal is to utilise these skills in a full time role continually learning and contributing to \
- create exciting opportunities. Please enjoy.'
+ web development portfolio displaying the use of html5, CSS3 and modern Javascript languages supported with node.js modules \
+ and webpack configuration. My goal is to continue to build on these skills, learning and \
+ creating exciting opportunities. Please enjoy.'
 
 const minimiseButt = document.createElement('button')
 minimiseButt.textContent = 'close'
